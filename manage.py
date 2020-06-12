7d5bd4ece1fd3dbe23e3e83c8d79f205ffadcf80
muutax <leva.1997@yandex.ru>
2020-06-13 00:03:54 +0700

    obnovka

--- manage.py
+++ manage.py
@@ -13,7 +13,8 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        )    execute_from_command_line(sys.argv)
+        )
+    execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
