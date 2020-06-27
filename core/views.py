7001e0ade7a1e72b2bb1b1da3330c5eabcb46bac
muutax <leva.1997@yandex.ru>
2020-06-27 18:28:52 +0700

    ...

--- core/views.py
+++ core/views.py
@@ -36,13 +36,9 @@ class RecordUpdateView(UpdateView):
         return super().get_context_data(**kwargs)
 
 class RecordDeleteView(DeleteView):
-    Records.objects.filter(id=id).delete()
+    model = Records
+    template_name = 'edit_page.html'
     success_url = reverse_lazy('edit_page')
-
-#def delete_page(request, id):
-#    get_record = Records.objects.get(id=id)
-#    get_record.delete()
-#    return redirect(reverse('edit_page'))
 
 class LoginView(LoginView):
     template_name = 'login.html'
